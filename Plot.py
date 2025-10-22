1a0ffcb994c2bb9734ad33a346e9b2f2583fa0e5
mjaabrookes <19050037@brookes.ac.uk>
2025-10-22 11:24:48 +0100

    Update Plot.py
    Updated it to accept the other data files too

--- Plot.py
+++ Plot.py
@@ -4,24 +4,23 @@ import re
 with open('try_distance.py', 'r') as f:
     data = f.read()
 
-#Get the values from robotFrames
+#Get the angle values from robotFrames
 angles = []
+frame_indices = []
 for line in data.split('\n'):
     if 'Frame2D.fromXYA' in line:
+        #Extract the frame index from the start of the line
+        index_match = re.search(r'\((\d+),', line)
         #Extract the angle
-        match = re.search(r'fromXYA\(([-\d.]+),([-\d.]+),([-\d.]+)\)', line)
-        if match:
-            angle = match.group(3)
-            angles.append(float(angle))
-
-#Pull out the timestamps
-timestamps = re.findall(r'\((\d+), ([\d.]+)\)', data)
-frame_nums = [int(t[0]) for t in timestamps]
+        angle_match = re.search(r'fromXYA\(([-\d.]+),([-\d.]+),([-\d.]+)\)', line)
+        if index_match and angle_match:
+            frame_indices.append(int(index_match.group(1)))
+            angles.append(float(angle_match.group(3)))
 
 #Make the plot
 plt.figure(figsize=(12, 5))
 plt.subplot(1, 1, 1)
-plt.plot(frame_nums, angles)
+plt.plot(frame_indices, angles)
 plt.xlabel('Frame')
 plt.ylabel('Angle')
 plt.title('Robot Angles')
